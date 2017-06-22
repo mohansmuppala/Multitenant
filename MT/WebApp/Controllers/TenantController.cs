80bb43a5f00a717dc9c6fec68330921b1a0a7387
Mohan <mohans.muppala@gmail.com>
2017-06-22 00:20:07 -0400

    added tenant data and functionality

---
+++ MT/WebApp/Controllers/TenantController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class TenantController : Controller
+    {
+        public ActionResult Index()
+        {
+            var tenants = new List<Tenant>
+            {
+                new Tenant()
+                {
+                    Name = "SVCC",
+                    DomainName = "www.siliconvalley-codecamp.com",
+                    Id = 1,
+                    Default = true
+                },
+                new Tenant()
+                {
+                    Name = "ANGU",
+                    DomainName = "www.angularu.com",
+                    Id = 1,
+                    Default = true
+                },
+                new Tenant()
+                {
+                    Name = "CSSC",
+                    DomainName = "www.codestarsummit.com",
+                    Id = 1,
+                    Default = true
+                }
+            };
+            return View(tenants);
+        }
+        
+    }
+}
