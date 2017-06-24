498a0691facdea55b9acdb82c627a76099304c1e
Mohan <mohans.muppala@gmail.com>
2017-06-23 23:17:01 -0400

    Added Properties to Models

--- MT/WebApp/Controllers/SessionController.cs
+++ MT/WebApp/Controllers/SessionController.cs
@@ -8,35 +8,11 @@ using WebApp.Models;
 
 namespace WebApp.Controllers
 {
-    public class TenantController : Controller
+    public class SessionController : Controller
     {
         public ActionResult Index()
         {
-            var tenants = new List<Tenant>
-            {
-                new Tenant()
-                {
-                    Name = "SVCC",
-                    DomainName = "www.siliconvalley-codecamp.com",
-                    Id = 1,
-                    Default = true
-                },
-                new Tenant()
-                {
-                    Name = "ANGU",
-                    DomainName = "www.angularu.com",
-                    Id = 1,
-                    Default = true
-                },
-                new Tenant()
-                {
-                    Name = "CSSC",
-                    DomainName = "www.codestarsummit.com",
-                    Id = 1,
-                    Default = true
-                }
-            };
-            return View(tenants);
+            return View();
         }
         
     }
--- MT/WebApp/Models/Session.cs
+++ MT/WebApp/Models/Session.cs
@@ -7,5 +7,18 @@ namespace WebApp.Models
 {
     public class Session
     {
+        public int Id { get; set; }
+
+        public int SessionId { get; set; }
+
+        public string Title { get; set; }
+
+        public string DescriptionShort { get; set; }
+
+        public string Description { get; set; }
+
+        public Tenant Tenant { get; set; }
+
+        public List<Speaker> Speakers { get; set; }
     }
 }
