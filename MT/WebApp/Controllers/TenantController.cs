ab625c3f6cb7d3ae71f075ce7a76103a1cccec88
Mohan <mohans.muppala@gmail.com>
2017-06-23 23:46:55 -0400

    Adding Async/Await to tenant controlelr

--- MT/WebApp/Controllers/TenantController.cs
+++ MT/WebApp/Controllers/TenantController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using WebApp.Models;
@@ -10,11 +12,11 @@ namespace WebApp.Controllers
 {
     public class TenantController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             using (var context = new MultiTenantContext())
             {
-                var tenants = context.Tenants.ToList();
+                var tenants = await context.Tenants.ToListAsync();
                 return View(tenants);
             }
 
